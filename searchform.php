84646b60a808fad2c56101f7760cbc9957289847
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2010-10-26 00:53:02 -0700

    adding a blank template with html source instead of display_post so that people can custom their own pages.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php home_url(); ?>/">
+<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
 	<div>
 		<input type="text" value="<?php _e('Search...','easel'); ?>" name="s" id="s-search" onfocus="this.value=(this.value=='<?php _e('Search...','easel'); ?>') ? '' : this.value;" onblur="this.value=(this.value=='') ? '<?php _e('Search...','easel'); ?>' : this.value;" />
 		<input id="search-btn" value="Search" type="image" src="<?php echo easel_themeinfo('themeurl'); ?>/images/search-small.png" class="search" />
